eaf03d645d46f66e5edb173b3abfb474d25f8de4
ElibethEP <elibeth.edlyn@gmail.com>
2025-07-01 00:05:04 -0500

    Update bs_datos.sql

--- bs_datos.sql
+++ bs_datos.sql
@@ -12,3 +12,12 @@ CREATE TABLE pacientes (
 );
 
 select * from pacientes;
+
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  secret VARCHAR(32)
+);
+select *from  usuarios;
+/* delete from usuarios where id = 2 */
